ac42de99d0f4c09df8a7144a2defd43010c63b35
unknown <llequang2002@gmail.com>
2022-10-09 03:58:55 +0200

    deleted useless files and changed the dynamic

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
+const snacks = [
+  {name: "Chips", sweat_or_salty: "salty"},
+  {name: "Brownie", sweat_or_salty: "sweet"},
+  {name: "Waffle", sweat_or_salty: "sweet"},
+  {name: "Sausage", sweat_or_salty: "salty"},
+  {name: "Bread", sweat_or_salty: "salty"},
+]
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(3000, function() {
+app.get('/Snacks/:id', (req, res) => {
+  res.json(snacks[parseInt(req.params.id) - 1])
+})
+
+app.listen(port, function() {
   console.log("Example app listening on port 3000 !")
 })
